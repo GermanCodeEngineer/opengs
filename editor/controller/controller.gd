4019f0871b0b0720e868853961548d43265c0856
GermanCodeEngineer <GermanCodeEngineer@proton.me>
2026-03-11 18:37:24 +0100

    slight refractor

--- editor/controller/controller.gd
+++ editor/controller/controller.gd
@@ -2,33 +2,33 @@ extends Node3D
 
 signal province_selected
 
-#Nodes
+# Nodes
 @onready var camera: Camera3D = $CameraSocket/Camera3D
 @onready var camera_socket: Node3D = $CameraSocket
 
-#Camera move
+# Camera move
 var camera_touchpad_move:Vector2 = Vector2.ZERO
 @export_range(0,1000,1) var camera_move_speed:float = 500.0
 
-#Camera rotate
+# Camera rotate
 var camera_rotation_direction:float = 0
 @export_range(0,10,0.1) var camera_rotation_speed:float = 0.20
 @export_range(0,20,1) var camera_base_rotation_speed:float = 6
 @export_range(0,10,1) var camera_socket_rotation_x_min:float = -1.60
 @export_range(0,10,1) var camera_socket_rotation_x_max:float = -0.20
 
-#Camera pan
+# Camera pan
 @export_range(0,32,4) var camera_automatic_pan_margin:int = 16
 @export_range(0,20,0.5) var camera_automatic_pan_speed:float = 18
 
-#Camera zoom
+# Camera zoom
 var camera_zoom_direction:float = 0
 @export_range(0,1000,1) var camera_zoom_speed:float = 1000.0
 @export_range(0,100,1) var camera_zoom_min:float = 40.0
 @export_range(0,1000,1) var camera_zoom_max:float = 1000.0
 @export_range(0,2,1) var camera_zoom_speed_damp:float = 0.92
 
-#flags
+# flags
 var camera_can_process:bool = true
 var camera_can_move_base:bool = true
 var camera_can_zoom:bool = true
@@ -37,7 +37,7 @@ var camera_can_rotate_base:bool = true
 var camera_can_rotate_socket_x:bool = true
 var camera_can_rotate_by_mouse_offfset:bool = true
 
-#Internal flag
+# Internal flag
 var camera_is_rotating_base:bool = false
 var camera_is_rotating_mouse:bool = false
 var mouse_last_position:Vector2 = Vector2.ZERO
@@ -56,7 +56,7 @@ func _process(delta:float) -> void:
 	camera_rotate_to_mouse_offsets(delta)
 
 
-#Moves the base of camera
+# Moves the base of camera
 func camera_base_move(delta:float) -> void:
 	if !camera_can_move_base: return
 	var velocity_direction: Vector3 = Vector3.ZERO
@@ -77,12 +77,12 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("Exit"):
 		get_tree().quit()
 	
-	#Camera Move
+	# Camera Move
 	if event is InputEventPanGesture:
 		camera_touchpad_move = event.delta
 
 	
-	#Camera Zoom
+	# Camera Zoom
 	if event.is_action("camera_zoom_in"):
 		camera_zoom_direction = -1
 	elif  event.is_action("camera_zoom_out"):
@@ -91,7 +91,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera_zoom_direction = 1-event.factor
 	
 	
-	#Camera rotations
+	# Camera rotations
 	if event.is_action_pressed("camera_rotate_right"):
 		camera_rotation_direction = -1
 		camera_is_rotating_base = true
@@ -127,18 +127,18 @@ func camera_rotate_to_mouse_offsets(delta:float) -> void:
 	
 	mouse_last_position = get_viewport().get_mouse_position()
 	
-	#camera_base_rotate_left_right(delta,mouse_offset.x) #Remove comment to get y rotation on mouse
+	# camera_base_rotate_left_right(delta,mouse_offset.x) # Remove comment to get y rotation on mouse
 	camera_socket_rotate_x(delta,mouse_offset.y)
 	
 	
-#Rotates the camera base
+# Rotates the camera base
 func camera_base_rotate(delta:float) -> void:
 	if !camera_can_rotate_base or !camera_is_rotating_base : return
 	
-	#To rotate
+	# To rotate
 	camera_base_rotate_left_right(delta, camera_rotation_direction * camera_base_rotation_speed)
 
-#Rotates the socket of the camera
+# Rotates the socket of the camera
 func camera_socket_rotate_x(delta:float, dir:float) -> void:
 	if !camera_can_rotate_socket_x  : return
 	
@@ -148,7 +148,7 @@ func camera_socket_rotate_x(delta:float, dir:float) -> void:
 	new_rotation_x = clamp(new_rotation_x,camera_socket_rotation_x_min,camera_socket_rotation_x_max)
 	camera_socket.rotation.x = new_rotation_x
 	
-#Rotates the camera speed left or right
+# Rotates the camera speed left or right
 func camera_base_rotate_left_right(delta:float, dir:float) -> void:
 	rotation.y += dir * camera_rotation_speed * delta
 	
@@ -157,21 +157,21 @@ func camera_automatic_pan(delta:float) -> void:
 	if !camera_can_automatic_pan: return
 	
 	var viewport_current:Viewport = get_viewport()
-	var pan_direction:Vector2 = Vector2(-1,-1) #Starts negative
+	var pan_direction:Vector2 = Vector2(-1,-1) # Starts negative
 	var viewport_visible_rectangle:Rect2i = Rect2i(viewport_current.get_visible_rect())
 	var viewport_size:Vector2i = viewport_visible_rectangle.size
 	var current_mouse_position:Vector2 = viewport_current.get_mouse_position()
-	var margin:float = camera_automatic_pan_margin #Shortcut var
+	var margin:float = camera_automatic_pan_margin # Shortcut var
 	
 	var zoom_factor:float = camera.position.z * 0.1
 	
-	#X pan
+	# X pan
 	if ((current_mouse_position.x < margin) or (current_mouse_position.x > viewport_size.x - margin)):
 		if current_mouse_position.x > viewport_size.x/2.0:
 			pan_direction.x = 1
 		translate(Vector3(pan_direction.x * delta * camera_automatic_pan_speed * zoom_factor,0,0))
 	
-	#Y pan
+	# Y pan
 	if ((current_mouse_position.y < margin) or (current_mouse_position.y > viewport_size.y - margin)):
 		if current_mouse_position.y > viewport_size.y/2.0:
 			pan_direction.y = 1
